69223f9bcc5dbb739f2552909eb56e7558e14318
Gerard <gerardmijares39@gmail.com>
2021-12-07 18:11:38 +0700

    troubleshooting token verification error

--- src/Login.js
+++ src/Login.js
@@ -1,72 +1,53 @@
-import React, { useState } from "react";
-import { setUserSession } from "./service/AuthService";
-import axios from "axios";
-
-const loginUrl =
-  "https://xdhylrg4nh.execute-api.us-east-1.amazonaws.com/prod/login";
+import React, {useState} from 'react';
+import axios from 'axios';
+import { setUserSession } from './service/AuthService'
+const loginAPIUrl = 'https://xdhylrg4nh.execute-api.us-east-1.amazonaws.com/prod/login';
 
 const Login = (props) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
 
   const submitHandler = (event) => {
     event.preventDefault();
-    if (username.trim() === "" || password.trim() === "") {
-      setErrorMessage("All fields are required");
+    if (username.trim() === '' || password.trim() === '') {
+      setErrorMessage('Both username and password are required');
       return;
     }
     setErrorMessage(null);
     const requestConfig = {
       headers: {
-        "x-api-key": "u8NeUf6akQaClOW3IkTdx5fU7IDaCjRJ4fy54edB",
-      },
-    };
-
+        'x-api-key': 'u8NeUf6akQaClOW3IkTdx5fU7IDaCjRJ4fy54edB'
+      }
+    }
     const requestBody = {
       username: username,
-      password: password,
-    };
-    axios
-      .post(loginUrl, requestBody, requestConfig)
-      .then((response) => {
-        setUserSession(response.data.user, response.data.token);
-        props.history.push("/profile");
-      })
-      .catch((error) => {
-        if (error.response.status === 401 || error.response.status === 403) {
-          setErrorMessage(error.response.data.message);
-        } else {
-          setErrorMessage(
-            "Sorry the server may be unavailable. Please try again later"
-          );
-        }
-      });
-  };
+      password: password
+    }
+
+    axios.post(loginAPIUrl, requestBody, requestConfig).then((response) => {
+      setUserSession(response.data.user, response.data.token);
+      props.history.push('/profile');
+    }).catch((error) => {
+      if (error.response.status === 401 || error.response.status === 403) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage('sorry....the backend server is down. please try again later!!');
+      }
+    })
+  }
 
   return (
     <div>
       <form onSubmit={submitHandler}>
         <h5>Login</h5>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <br />
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <br />
-        <input type="submit" value="login" />
+        username: <input type="text" value={username} onChange={event => setUsername(event.target.value)} /> <br/>
+        password: <input type="password" value={password} onChange={event => setPassword(event.target.value)} /> <br/>
+        <input type="submit" value="Login" />
       </form>
       {errorMessage && <p className="message">{errorMessage}</p>}
     </div>
-  );
-};
+  )
+}
 
 export default Login;
--- src/MyProfile.js
+++ src/MyProfile.js
@@ -1,112 +1,20 @@
-import React, { useState, useEffect } from "react";
-import {
-  getToken,
-  getUser,
-  resetUserSession,
-  setUserSession,
-} from "./service/AuthService";
-import axios from "axios";
+import React from 'react';
+import { getUser, resetUserSession } from './service/AuthService';
 
 const MyProfile = (props) => {
-  const profileURL =
-    "https://xdhylrg4nh.execute-api.us-east-1.amazonaws.com/prod/profile";
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [penname, setPenname] = useState("");
-  const [message, setMessage] = useState(null);
-
   const user = getUser();
-
-  setName(user.name);
+  const name = user !== 'undefined' && user ? user.name : '';
 
   const logoutHandler = () => {
     resetUserSession();
-    props.history.push("/login");
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (
-      name.trim() === "" ||
-      email.trim() === "" ||
-      username.trim() === "" ||
-      penname.trim() === ""
-    ) {
-      setMessage("All fields are required");
-      return;
-    }
-    setMessage(null);
-    const requestConfig = {
-      headers: {
-        "x-api-key": "u8NeUf6akQaClOW3IkTdx5fU7IDaCjRJ4fy54edB",
-      },
-    };
-
-    const requestBody = {
-      token: getToken(),
-      username: username,
-      email: email,
-      name: name,
-      penname: penname,
-    };
-    axios
-      .post(profileURL, requestBody, requestConfig)
-      .then((response) => {
-        setMessage("Update Successful");
-      })
-      .catch((error) => {
-        if (error.response.status === 401 || error.response.status === 403) {
-          setMessage(error.response.data.message);
-        } else {
-          setMessage(
-            "Sorry the server may be unavailable. Please try again later"
-          );
-        }
-      });
-  };
-
+    props.history.push('login');
+  }
   return (
     <div>
-      Hello {name}! You can update your profile below: &nbsp;
+      Hello {name}! You are logged in. <br />
       <input type="button" value="Logout" onClick={logoutHandler} />
-      <br />
-      <form onSubmit={submitHandler}>
-        <h5>Register</h5>
-        Full Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-        <br />
-        E-mail:
-        <input
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <br />
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <br />
-        Pen Name:
-        <input
-          type="text"
-          value={penname}
-          onChange={(event) => setPenname(event.target.value)}
-        />
-        <br />
-        <input type="submit" value="register" />
-      </form>
-      {message && <p className="message">{message}</p>}
     </div>
-  );
-};
+  )
+}
 
 export default MyProfile;
